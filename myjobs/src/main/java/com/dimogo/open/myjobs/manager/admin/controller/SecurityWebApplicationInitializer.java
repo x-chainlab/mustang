8eaf39f55cc569c35f22b12851c720d1fa264fcc
meeler <524414419@qq.com>
2017-05-09 16:28:10 +0800

    add spring-security

--- myjobs/src/main/java/com/dimogo/open/myjobs/manager/admin/controller/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.dimogo.open.myjobs.manager.admin.controller;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by Auser on 2017/5/8.
- */
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
