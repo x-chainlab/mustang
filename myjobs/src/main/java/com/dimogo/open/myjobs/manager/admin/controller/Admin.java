c4f4355513f89df8ba902db8de0211a6364693f1
ethan-xiao <308056554@qq.com>
2017-05-12 23:07:11 +0800

    增加登录失败提示信息

--- myjobs/src/main/java/com/dimogo/open/myjobs/manager/admin/controller/Admin.java
+++ myjobs/src/main/java/com/dimogo/open/myjobs/manager/admin/controller/Admin.java
@@ -1,9 +1,12 @@
 package com.dimogo.open.myjobs.manager.admin.controller;
 
 import com.dimogo.open.myjobs.manager.admin.service.MyJobsService;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by ethanx on 2017/5/10.
@@ -14,7 +17,10 @@ public class Admin {
 	private MyJobsService service;
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String index() {
+	public String index(ModelMap model, @RequestParam(required=false, defaultValue = "", value = "error") String error) {
+		if ("loginFailure".equalsIgnoreCase(error)) {
+			model.addAttribute("error", "Invalid user name/password");
+		}
 		return "index";
 	}
 
--- myjobs/src/main/resources/org/springframework/batch/admin/web/manager/admin/html/index.ftl
+++ myjobs/src/main/resources/org/springframework/batch/admin/web/manager/admin/html/index.ftl
@@ -2,6 +2,8 @@
 <div id="login" style="margin: auto;">
     <form action="/login" method="post">
         <ol>
+        <#if error??>
+            <li><label for="msg">Message</label><span id="msg"><font color="red">${error}</font></span></li></#if>
             <li><label for="username">User Name</label><input id="username" name="username" type="text"></li>
             <li><label for="password">Password</label><input id="password" name="password" type="password"></li>
             <li><input type="submit" value="Login"></li>
