fe84f756f28bba4baacff637159811fa104d92c2
ethan-xiao <308056554@qq.com>
2017-11-08 16:21:59 +0800

    fixed: 解决placeholderProPerties加载自定义配置文件的问题，覆盖placeholderProperties定义，配置文件目录：resource/conf/*.properties

--- myjobs/src/main/java/com/dimogo/open/myjobs/test/HelloTasklet.java
+++ myjobs/src/main/java/com/dimogo/open/myjobs/test/HelloTasklet.java
@@ -1,7 +1,5 @@
 package com.dimogo.open.myjobs.test;
 
-import com.dimogo.open.myjobs.utils.JobInstanceUtils;
-import com.dimogo.open.myjobs.utils.JobUtils;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -11,9 +9,16 @@ import org.springframework.batch.repeat.RepeatStatus;
  * Created by Ethan Xiao on 2017/3/31.
  */
 public class HelloTasklet implements Tasklet {
+
+	private String message = "Hello Tasklet, My Jobs";
+
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-		Thread.sleep(3 * 60 * 1000);
-		System.out.println("Hello Tasklet, My Jobs");
+		//Thread.sleep(3 * 60 * 1000);
+		System.out.println(message);
 		return null;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 }
